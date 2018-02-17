5f88a9bc74017ec697923fe19ba4910a818f98fc
Sharkie <craig.sharkie@gmail.com>
2018-02-18 07:57:00 +1100

    Generating revision lists programatically
    - Added Array to hold revision week ranges, E.G. "2-5"
    - Concatenating the `ROM.weeks` weeks into a new property that matched the range and contains all the words from the ranges weeks,  E.G. ROM.weeks["2-5"]

--- data/rom.js
+++ data/rom.js
@@ -1,20 +1,37 @@
 (function(){
   const ROM = {
+    revisions: ["2-5"],
     weeks: {
-      "1": [
+      "2": [
         "jaw", "straw", "dawn", "awful", "yawn", "able", "energy", "always", "circle", "daughter", "weak", "week", "sunset", "fiction", "comb", "enjoy", "mould", "awoke", "dream", "tried"
       ],
-      "2": [
+      "3": [
         "ready", "heavy", "spread", "instead", "breakfast", "asked", "salt", "itself", "person", "chalk", "grumble", "another", "past", "passed", "change", "surprise", "sorry", "active", "knows", "nose"
       ],
-      "3": [
-        "fry", "sky", "cry", "spy", "dry", "very", "every", "pretty", "ugly", "family", "mail", "male", "tail", "tale", "pail", "pale", "night", "knight", "travel", "robber"
-      ],
       "4": [
+        "fly", "sky", "cry", "spy", "dry", "very", "every", "pretty", "ugly", "family", "mail", "male", "tail", "tale", "pail", "pale", "night", "knight", "travel", "robber"
+      ],
+      "5": [
         "prize", "twice", "alike", "invite", "alive", "hour", "our", "habit", "thunder", "behind", "beautiful", "story", "happened", "hear", "amuse", "omen", "proper", "wren", "wrong", "knock"
       ]
+    },
+    setup: function () {
+      for (const revision in ROM.revisions) {
+        let list = [];
+        const range = ROM.revisions[revision].split("-");
+
+        while (range[0] <= range[1]) {
+          list = list.concat(ROM.weeks[range[0]]);
+          range[0]++;
+        }
+        ROM.weeks[ROM.revisions[revision]] = list;
+      }
+    },
+    init: function () {
+      this.setup();
     }
   }
 
   window.ROM = ROM;
+  ROM.init();
 }());
